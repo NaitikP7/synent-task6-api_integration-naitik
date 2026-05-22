d48e5b99f72b5f137711dceae68da3063feb3282
NaitikP7 <24it071@charusat.edu.in>
2026-05-22 15:32:35 +0530

    improved responsiveness and polished UI

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,7 +10,7 @@ const Card = ({id, repos}) => {
         <p className='font-medium text-lg'>{repos.name}</p>
         <a href={repos.clone_url} target='_blank'><SquareArrowOutUpRight className='' /></a>
       </div>            
-      <p className='leading-tight text-sm line-clamp-4 text-gray-300' className={Response.description?"block":"hidden"}>{repos.description}</p>
+      <p className={`leading-tight text-sm line-clamp-4 text-gray-300 font-medium ${repos.description ? "block" : "hidden"}`}>{repos.description}</p>
       <p>Language : {repos.language}</p>
       <p>Stars : {repos.stargazers_count}</p>
       <p>Updated at : {formatted} </p> 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import git from '../assets/github.webp'
 import Searchbar from './Searchbar'
 import './navbar.css'
+import { Fan } from 'lucide-react';
 const Navbar = (props) => {
   return (
     <div id='navbar' className='flex justify-between 
     py-6 min-[480px]:px-8    
     lg:px-20 items-center backdrop-blur-xl'>
-        <h2 className='lg:text-[20px]'>DevSearch</h2>
+      <div className='flex items-center gap-1'>
+        <Fan strokeWidth={3} />
+        <h2 className='lg:text-[20px] font-bold'>Dev<span className='font-normal text-shadow-lime-50'>Search</span></h2>
+      </div>
+        
         <Searchbar username={props.username} setUsername ={props.setUsername} fetchUser={props.fetchUser} 
         />
         <a href="https://github.com/" className='h-12 w-12 rounded-full items-center justify-center shadow-[0_0_25px_rgba(255,63,0,0.7)]
